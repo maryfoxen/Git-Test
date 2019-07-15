9e51ea8707e12891df0dc3e4b55a5d2383576c5f
Girls Code <GWC@cs81-gwc-02.local>
2019-07-15 09:39:20 -0400

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Girls Who Code on 7/15/19.
+//  Copyright © 2019 Mary Foxen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
